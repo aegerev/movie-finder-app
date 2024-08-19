7d2f282b76dedecb5d694e7c186666566c0de745
alex <alexandersegerev@gmail.com>
2024-08-19 11:41:42 -0400

    Added moviesDAO + made index.js suitable for Render

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
 import app from './server.js'
 import mongodb from "mongodb"
 import dotenv from 'dotenv'
+import MoviesDAO from './dao/moviesDAO.js'
 
 async function main(){
     dotenv.config()
 
     const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI)
-    const port = process.env.PORT || 8000
+    const port = process.env.PORT || 3001;
 
     try {
         await client.connect()
-
+        await MoviesDAO.injectDB(client)
+        
         app.listen(port, () => {
             console.log('server is running on port' + port);
         })
