109291c69ffa0a79128b58e12c44fdd207d3b8b1
alex <alexandersegerev@gmail.com>
2024-08-20 10:12:35 -0400

    Added getMovieByID and getMovieRatings.

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -4,5 +4,7 @@ import MoviesController from './movies.controller.js'
 const router = express.Router()
 
 router.route('/').get(MoviesController.apiGetMovies)
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
+router.route("/ratings").get(MoviesController.apiGetRatings)
 
 export default router;
